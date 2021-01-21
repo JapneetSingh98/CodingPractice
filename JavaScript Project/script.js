033746ea73bcd3f8d8688509e83b315d13e4f2b0
Japneet Singh <japneetsingh@gmail.com>
2021-01-20 20:56:54 -0800

    javascript project

---
+++ JavaScript Project/script.js
@@ -0,0 +1,12 @@
+function revealMessage(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function hideMessage(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+function hwMessage() {
+    var mess = "Hello World";
+    return mess;
+}
