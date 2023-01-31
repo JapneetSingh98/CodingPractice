36fdfdeec5d777971deacb28f8b83b750090257d
Japneet Singh <japneetsingh@gmail.com>
2023-01-30 18:27:36 -0800

    Added scoring

--- Python_Projects/Pong/pong.py
+++ Python_Projects/Pong/pong.py
@@ -7,7 +7,6 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# paddle
 def create_paddle(x_pos):
     paddle = turtle.Turtle()
     paddle.speed(0)
@@ -16,6 +15,7 @@ def create_paddle(x_pos):
     paddle.color("white")
     paddle.penup()
     paddle.goto(x_pos, 0)
+    paddle.score = 0
     return paddle
 
 def create_ball():
@@ -25,9 +25,24 @@ def create_ball():
     ball.color("white")
     ball.penup()
     ball.goto(0, 0)
-    ball.dx = 0.5
-    ball.dy = 0.5
+    ball.dx = 0.65
+    ball.dy = 0.65
     return ball
+
+def create_scoreboard():
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("white")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 260)
+    pen.write("Player A: 0, Player B: 0", align="center", font=("Courier", 24, "normal"))
+    return pen
+
+def update_scoreboard(scoreboard, paddleA, paddleB):
+    scoreboard.clear()
+    scoreboard.write("Player A: {}, Player B: {}".format(paddleA.score, paddleB.score),
+                     align="center", font=("Courier", 24, "normal"))
 
 def move_paddle(paddle, direction):
     y = paddle.ycor()
@@ -42,12 +57,17 @@ def check_top_bottom_border(ball):
     if ball.ycor() > 290 or ball.ycor() < -290:
         ball.dy *= -1
 
-def check_left_right_border(ball):
-    # Ball goes past paddles
-    # TODO add scoring
-    if ball.xcor() > 390 or ball.xcor() < -390:
-        ball.goto(0, 0)
-        ball.dx *= -1
+def check_left_right_border(ball, paddleA, paddleB, scoreboard):
+    if ball.xcor() > 390:
+        paddle = paddleA
+    elif ball.xcor() < -390:
+        paddle = paddleB
+    else:
+        return
+    ball.goto(0, 0)
+    ball.dx *= -1
+    paddle.score += 1
+    update_scoreboard(scoreboard, paddleA, paddleB)
 
 def check_paddle_bounce(ball, paddleA, paddleB):
    if ball.xcor() > 330 and ball.xcor() < 340:
@@ -64,7 +84,7 @@ def check_paddle_bounce(ball, paddleA, paddleB):
 paddleA = create_paddle(-350)
 paddleB = create_paddle(350)
 ball = create_ball()
-
+scoreboard = create_scoreboard()
 
 wn.listen()
 wn.onkeypress(functools.partial(move_paddle, paddleA, "up"), "w")
@@ -80,5 +100,5 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     check_top_bottom_border(ball)
-    check_left_right_border(ball)
+    check_left_right_border(ball, paddleA, paddleB, scoreboard)
     check_paddle_bounce(ball, paddleA, paddleB)
