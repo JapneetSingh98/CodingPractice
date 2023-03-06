3d84a33b22724fd339d6939e901979342e86a0ab
Japneet Singh <japneetsingh@gmail.com>
2023-03-05 20:58:02 -0800

    90.4 percentile runtime, 81.87 percentile memory

--- LeetCode/buySellStock.py
+++ LeetCode/buySellStock.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        curProfit = 0
-        curMin = float('inf')
-        
-        for i in range(len(prices)):
-            
-            curProfit = max(curProfit, prices[i]-curMin)
-            curMin = min(curMin, prices[i])
-        
-        return curProfit
+        curMax = 0
+        buyPrice = None
+
+        for price in prices:
+            if buyPrice == None or price < buyPrice:
+                buyPrice = price
+            else:
+                curProfit = price - buyPrice
+                if curProfit > curMax:
+                    curMax = curProfit
+        return curMax
